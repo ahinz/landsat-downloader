9d1adaf3516dd08c0a11568fe1eb0814d8fe9395
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-20 17:37:29 -0400

    Create dictionary with keys = datasets, values = years available

--- scripts/xml_downloader.py
+++ scripts/xml_downloader.py
@@ -3,7 +3,9 @@
 import urllib2
 from bs4 import BeautifulSoup
 import re
-import multiprocessing
+from multiprocessing import Pool
+import os
+import cPickle as pickle
 
 mcd_top = 'http://e4ftl01.cr.usgs.gov/MODIS_Composites/MOTA/'
 
@@ -17,35 +19,44 @@ def url_finder(url, regex):
             links.append(url+tag['href'])
     return links
 
+def file_diff(file1, file2):
+    check_exists(file2)
+    f1 = open(file1, 'r')
+    f2 = open(file2, 'r')
+    f1list = f1.read().split('\n')
+    f2list = f2.read().split('\n')
+    f1set = set(f1list)
+    f2set = set(f2list)
+    uniq = f1set.difference(f2set)
+    return list(uniq)
+
+def check_exists(f):
+    if os.path.exists(f):
+        pass
+    else:
+        os.system('touch {0}'.format(f))
+
 if __name__ == '__main__':
 
     # Get links for all MCD Directories #
     print "Getting MCD links"
+    mota = 'http://e4ftl01.cr.usgs.gov/MODIS_Composites/MOTA/'
     mcd_links = url_finder('http://e4ftl01.cr.usgs.gov/MODIS_Composites/MOTA/', 'MCD')
-    print "Found {0} MCD links".format(mcd_links)
-    year_links = []
-
+    mcd_dict = {}
+    for l in mcd_links:
+        name = l.split('/')[-2]
+        mcd_dict[name] = []
+        mcd_dict[name].append(l)
+    print "Found {0} MCD links".format(len(mcd_links))
+    print mcd_dict
     print "Getting Year links"
-    for counter, l in enumerate(mcd_links):
-        print counter
-        year_links += url_finder(l, '\d+')
-
+    for counter, l in enumerate(mcd_dict.keys()):
+        urls = url_finder(mota + l, '\d+')
+        for url in urls:
+            year = url.split('/')[-2]
+            mcd_dict[l].append(year)
     print "Finished getting year links"
 
-    print "Finding all XML links"
-    xml_links = []
+    mcd_output = open('./mcd_output.p', 'wb')
+    pickle.dump(mcd_dict, mcd_output)
     
-    for counter, y in enumerate(year_links):
-        print counter
-        xml_links += url_finder(y, 'xml$')
-
-    print "Need to download {0} XML files".format(len(xml_links))
-    for counter, x in enumerate(xml_links):
-        if counter % 30 == 0:
-            print 'Finished downloading {0}'.format(counter)
-        p = urllib2.urlopen(x)
-        html = p.read()
-        fname = "___".join(x.split('/')[-3:])
-        output = open('./data/{0}'.format(fname), 'w')
-        output.write(html)
-        output.close()
