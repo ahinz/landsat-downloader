6efdf05e1c25bdf0d64de5c9caa401583e915939
Adam Hinz <hinz.adam@gmail.com>
2013-04-20 19:32:51 -0400

    Add in xml file download

--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ import os
 import hashlib
 import argparse
 from scripts import xml_downloader
+from scripts import dl_xml
 from bs4 import BeautifulSoup
 
 
@@ -142,21 +143,33 @@ def main():
 # sub-command functions
 def list_sets(args):
     adict = xml_downloader.get_dict()
+    dataset = None
     if args.dataset:
         try:
-            data = adict[adict.keys()[int(args.dataset)]]
+            dataset = adict.keys()[int(args.dataset)]
         except:
             matches = filter(lambda z: z.startswith(args.dataset), adict.keys())
             if len(matches) == 1:
-                data = adict[matches[0]]
+                dataset = matches[0]
             else:
                 print "Too many matches:"
                 print '\n'.join(matches)
                 exit(1)
-    else:
-        data = adict.keys()
 
-    print '\n'.join(["%03d:  %s" % (a,b) for (a,b) in zip(range(0,len(adict.keys())), data)])
+    if args.date and dataset:
+        dates = adict[dataset]
+
+        if args.date in dates:
+            dl_xml.download_xml(dataset, args.date)
+        else:
+            print "That date wasn't found"
+    else:
+        if dataset:
+            data = adict[dataset]
+        else:
+            data = adict.keys()
+
+        print '\n'.join(["%03d:  %s" % (a,b) for (a,b) in zip(range(0,len(adict.keys())), data)])
 
 if __name__=='__main__':
     #adict = xml_downloader.get_dict()
@@ -166,9 +179,10 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
-    # create the parser for the "foo" command
-    parser_list = subparsers.add_parser('list')
+    # create the parser for the "list" command
+    parser_list = subparsers.add_parser('info')
     parser_list.add_argument('dataset', type=str, nargs='?')
+    parser_list.add_argument('date', type=str, nargs='?')
     parser_list.set_defaults(func=list_sets)
 
     # parse the args and call whatever function was selected
--- scripts/dl_xml.py
+++ scripts/dl_xml.py
@@ -2,6 +2,7 @@
 from xml_downloader import url_finder
 import urllib2
 import sys
+import os
 
 def download_xml(dataset, year):
     """Downloads ALL XML dataset and writes to an output dir
@@ -15,17 +16,32 @@ def download_xml(dataset, year):
     xml_links = url_finder(url, 'xml$')
     print "Found {0} XML links".format(len(xml_links))
 
+    missing = False
+    for l in xml_links:
+        fname = "___".join(l.split('/')[-3:])
+        path = './tmp/{0}'.format(fname)
+        if not os.path.exists(path):
+            missing = True
+            break
+
+    if not missing:
+        return
+
     for counter, l in enumerate(xml_links):
         complete = int(100*(counter/float(len(xml_links))))
-        sys.stdout.write("\r{0} percent complete".format(complete))
+        sys.stdout.write("\r{0} percent complete ({1}/{2})".format(complete, counter, len(xml_links)))
         fname = "___".join(l.split('/')[-3:])
-        page = urllib2.urlopen(url+l)
-        html = page.read()
-        output = open('./tmp/{0}'.format(fname), 'w')
-        output.write(html)
-        output.close()
+        path = './tmp/{0}'.format(fname)
+        if not os.path.exists(path):
+            page = urllib2.urlopen(url+l)
+            html = page.read()
+            output = open(path, 'w')
+            output.write(html)
+            output.close()
+
         sys.stdout.flush()
-        print '\n'
+
+    print '\n'
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
