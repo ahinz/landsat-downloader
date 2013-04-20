5f42d27247b682b1a27e21796ecde91ecd33de81
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-20 19:15:59 -0400

    Added progress percentage complete

--- scripts/dl_xml.py
+++ scripts/dl_xml.py
@@ -13,13 +13,19 @@ def download_xml(dataset, year):
     """
     url = 'http://e4ftl01.cr.usgs.gov/MODIS_Composites/MOTA/{0}/{1}/'.format(dataset, year)
     xml_links = url_finder(url, 'xml$')
-    for l in xml_links:
+    print "Found {0} XML links".format(len(xml_links))
+
+    for counter, l in enumerate(xml_links):
+        complete = int(100*(counter/float(len(xml_links))))
+        sys.stdout.write("\r{0} percent complete".format(complete))
         fname = "___".join(l.split('/')[-3:])
-        page = urllib2.urlopen(l)
+        page = urllib2.urlopen(url+l)
         html = page.read()
         output = open('./tmp/{0}'.format(fname), 'w')
         output.write(html)
         output.close()
+        sys.stdout.flush()
+        print '\n'
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
