ffadea0d58ed7c2f5d0ac75f02a394c80deca1eb
Adam Hinz <hinz.adam@gmail.com>
2013-04-20 21:36:21 -0400

    Write xml files to a different directory

--- scripts/dl_xml.py
+++ scripts/dl_xml.py
@@ -16,32 +16,39 @@ def download_xml(dataset, year):
     xml_links = url_finder(url, 'xml$')
     print "Found {0} XML links".format(len(xml_links))
 
+    paths = []
     missing = False
     for l in xml_links:
         fname = "___".join(l.split('/')[-3:])
-        path = './tmp/{0}'.format(fname)
+        path = './tmp/xml/{0}'.format(fname)
         if not os.path.exists(path):
             missing = True
             break
 
+        paths.append(path)
+
     if not missing:
-        return
+        return paths
+    else:
+        paths = []
 
     for counter, l in enumerate(xml_links):
         complete = int(100*(counter/float(len(xml_links))))
         sys.stdout.write("\r{0} percent complete ({1}/{2})".format(complete, counter, len(xml_links)))
         fname = "___".join(l.split('/')[-3:])
-        path = './tmp/{0}'.format(fname)
+        path = './tmp/xml/{0}'.format(fname)
         if not os.path.exists(path):
             page = urllib2.urlopen(url+l)
             html = page.read()
             output = open(path, 'w')
+            paths.append(path)
             output.write(html)
             output.close()
 
         sys.stdout.flush()
 
     print '\n'
+    return paths
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
