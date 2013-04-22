ee423f483208eb591bf43e3e4f94ec7d57db6a4b
Christopher Brown <chrbrown@sas.upenn.edu>
2013-04-22 10:59:13 -0400

    Fixed as per Bennet's suggestions
    - Added exception handling on bad input
    - Used set to check for inclusion
    - Won't reprint options on bad input

--- scripts/get_bands.py
+++ scripts/get_bands.py
@@ -1,4 +1,5 @@
 import sys
+from sets import Set
 from osgeo import gdal
 
 def user_select_band(path_to_hdf, default=None):
@@ -7,23 +8,30 @@ def user_select_band(path_to_hdf, default=None):
 
     If user chooses default, takes first band.
     """
-    gdal_data = gdal.Open(path_to_hdf)
+    gdal_data = gdal.Open("{0}".format(path_to_hdf))
     bands = gdal_data.GetSubDatasets()
     if default != None:
         return default, bands[default][0]
 
     print 'Please enter a selection for band to use for this map:'
     selection = False
+
+    # Give Options to User
+    acceptable = Set(range(len(bands)))
+    for counter, band in enumerate(bands):
+        relevant = band[0].split(':')[-1]
+        print "{0}: {1}".format(counter, relevant)
+    
     while selection == False:
-        acceptable = []
-        for counter, band in enumerate(bands):
-            relevant = band[0].split(':')[-1]
-            print "{0}: {1}".format(counter, relevant)
-            acceptable.append(counter)
         user_input = raw_input("Please enter integer of your choice:")
-        user_input = int(user_input)
+
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print "Not an integer selection, please enter an integer"
+            continue
         if user_input in acceptable:
             selection = True
         else:
-            print "Error: Not acceptable selection, please enter an integer of one of the following choices"
+            print "Error: Not acceptable selection, please enter an integer."
     return user_input, bands[user_input][0]
