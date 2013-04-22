c08d0da1a23beffbf024b3efe4ffa87f66c3858d
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-21 20:04:50 -0400

    Moved to function, skip downloading if file exists.

--- scripts/dl_details.py
+++ scripts/dl_details.py
@@ -4,36 +4,53 @@ available map to download."""
 from bs4 import BeautifulSoup
 import urllib2
 import cPickle as pickle
+import sys, os
 
-product_table = 'https://lpdaac.usgs.gov/products/modis_products_table'
-products = urllib2.urlopen(product_table)
-soup = BeautifulSoup(products.read())
+def dl_details():
 
-base_url = 'https://lpdaac.usgs.gov'
+    """Downloads details on available MODIS data. Creates and saves
+    a dictionary with:
 
-datasets = soup.findAll('td', {"class": 'views-field views-field-title'})
+    keys = MODIS data name (e.g. MCD12Q1)
+    values = list of url, title, and detailed description"""
+    
+    if os.path.isfile('./description_of_maps.p'):
+        print "Already downloaded file descriptions"
+        return
+    
+    product_table = 'https://lpdaac.usgs.gov/products/modis_products_table'
+    products = urllib2.urlopen(product_table)
+    soup = BeautifulSoup(products.read())
 
-description_dict = {}
-for d in datasets:
-    description_dict[d.text.strip()] = [base_url+d.find('a')['href'], ]
+    base_url = 'https://lpdaac.usgs.gov'
 
-print "Downloading {0} dataset information.".format(len(description_dict.keys()))
-for counter, k in enumerate(description_dict.keys()):
-    print "Downloaded {0} files".format(counter)
-    url = description_dict[k][0]
-    try:
-        page = urllib2.urlopen(url)
-    except urllib2.URLError:
-        print "Could not get info for: {0} ({1})".format(k, url)
-        description_dict[k].append('')
-        description_dict[k].append('')
-        continue
-    soup = BeautifulSoup(page.read())
-    title = soup.find('div', {'class': 'field-item even'})
-    description_dict[k].append(title.text.strip())
-    paragraph = soup.find('div', {'property': 'content:encoded'}).text.strip()
-    description_dict[k].append(paragraph)
+    datasets = soup.findAll('td', {"class": 'views-field views-field-title'})
+    
+    description_dict = {}
+    for d in datasets:
+        description_dict[d.text.strip()] = [base_url+d.find('a')['href'], ]
 
-output = open('./description_of_maps.p', 'wb')
-pickle.dump(description_dict, output)
-output.close()
+    print "Downloading information for {0} maps.".format(len(description_dict.keys()))
+    for counter, k in enumerate(description_dict.keys()):
+        complete = int(100*(counter/float(len(description_dict.keys()))))
+        sys.stdout.write("\r{0} percent complete ({1}/{2})".format(complete, counter, len(description_dict.keys())))
+        url = description_dict[k][0]
+        try:
+            page = urllib2.urlopen(url)
+        except urllib2.URLError:
+            print "Could not get info for: {0} ({1})".format(k, url)
+            description_dict[k].append('')
+            description_dict[k].append('')
+            continue
+        soup = BeautifulSoup(page.read())
+        title = soup.find('div', {'class': 'field-item even'})
+        description_dict[k].append(title.text.strip())
+        paragraph = soup.find('div', {'property': 'content:encoded'}).text.strip()
+        description_dict[k].append(paragraph)
+
+    output = open('./description_of_maps.p', 'wb')
+    pickle.dump(description_dict, output)
+    output.close()
+
+if __name__ == '__main__':
+    dl_details()
