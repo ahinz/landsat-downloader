dda71a29541303951fe898bdf6bc6b27d8ec6979
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-26 16:06:47 -0400

    Output a JSON file instead of a pickle

--- scripts/dl_details.py
+++ scripts/dl_details.py
@@ -1,9 +1,9 @@
-"""Outputs a pickled dictionary with information on each
+"""Outputs a json file with information on each
 available map to download."""
 
 from bs4 import BeautifulSoup
 import urllib2
-import cPickle as pickle
+import json
 import sys, os
 
 def dl_details():
@@ -14,7 +14,7 @@ def dl_details():
     keys = MODIS data name (e.g. MCD12Q1)
     values = list of url, title, and detailed description"""
     
-    if os.path.isfile('./description_of_maps.p'):
+    if os.path.isfile('./description_of_maps.json'):
         print "Already downloaded file descriptions"
         return
     
@@ -28,28 +28,28 @@ def dl_details():
     
     description_dict = {}
     for d in datasets:
-        description_dict[d.text.strip()] = [base_url+d.find('a')['href'], ]
-
+        description_dict[d.text.strip()] = {}
+        description_dict[d.text.strip()]['url'] = base_url+d.find('a')['href']
     print "Downloading information for {0} maps.".format(len(description_dict.keys()))
     for counter, k in enumerate(description_dict.keys()):
         complete = int(100*(counter/float(len(description_dict.keys()))))
         sys.stdout.write("\r{0} percent complete ({1}/{2})".format(complete, counter, len(description_dict.keys())))
-        url = description_dict[k][0]
+        url = description_dict[k]['url']
         try:
             page = urllib2.urlopen(url)
         except urllib2.URLError:
             print "Could not get info for: {0} ({1})".format(k, url)
-            description_dict[k].append('')
-            description_dict[k].append('')
+            description_dict[k]['description'] = 'No Description'
+            description_dict[k]['title'] = 'No Title Available'
             continue
         soup = BeautifulSoup(page.read())
         title = soup.find('div', {'class': 'field-item even'})
-        description_dict[k].append(title.text.strip())
+        description_dict[k]['title'] = title.text.strip()
         paragraph = soup.find('div', {'property': 'content:encoded'}).text.strip()
-        description_dict[k].append(paragraph)
+        description_dict[k]['description'] = paragraph
 
-    output = open('./description_of_maps.p', 'wb')
-    pickle.dump(description_dict, output)
+    output = open('./description_of_maps.json', 'w')
+    output.write(json.dumps(description_dict))
     output.close()
 
 if __name__ == '__main__':
